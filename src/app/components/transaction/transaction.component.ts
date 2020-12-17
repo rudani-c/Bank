0b3481c2688afb40fcc55b744e7e23eccb92154a
rudani-c <rudani.c@gmail.com>
2020-12-17 16:39:45 -0500

    displayed transaction list

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { TransactionService } from "../../services/transaction.service";
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
-  selector: "transaction",
-  templateUrl: "./transaction.component.html",
-  styleUrls: ["./transaction.component.css"],
+  selector: 'transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css'],
 })
 export class TransactionComponent implements OnInit {
   transactionsArray = [];
@@ -20,5 +20,32 @@ export class TransactionComponent implements OnInit {
         alert(err);
       }
     );
+  }
+
+  getMerchantLogoUrl(merchantName: string) {
+    switch(merchantName) {
+      case 'Backbase':
+        return '/assets/icons/backbase.png';
+      case 'The Tea Lounge':
+        return '/assets/icons/the-tea-lounge.png';
+      case 'Texaco':
+        return '/assets/icons/texaco.png';
+      case 'Amazon Online Store':
+        return '/assets/icons/amazon-online-store.png';
+      case '7-Eleven':
+        return '/assets/icons/7-eleven.png';
+      case 'H&M Online Store':
+        return '/assets/icons/h&m-online-store.png';
+      case 'Jerry Hildreth':
+        return '/assets/icons/jerry-hildreth.png';
+      case 'Lawrence Pearson':
+        return '/assets/icons/lawrence-pearson.png';
+      case 'Whole Foods':
+        return '/assets/icons/whole-foods.png';
+      case 'Southern Electric Company':
+        return '/assets/icons/southern-electric-company.png';
+      default:
+        return '/assets/icons/backbase.png';
+    }
   }
 }
